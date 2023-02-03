ce99078e4411eea88ef7871013bd8e215b8eebc1
erindhoxha <hello@erindhoxha.dev>
2023-02-03 14:09:17 +1100

    transitions

--- components/Name/Name.js
+++ components/Name/Name.js
@@ -1,45 +1,64 @@
 import React from 'react';
 import { useCursorContext } from '../../pages/_app';
 import styles from './Name.module.css';
+import { motion } from 'framer-motion';
 
 const Name = () => {
   const { setCursorVariantFn } = useCursorContext();
   return (
     <div className={styles.nameContainer}>
       <div className="content">
-        <div>
-          <h1 className={styles.h1}>erind hoxha</h1>
-        </div>
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 1 }}
+        >
+          <div>
+            <h1 className={styles.h1}>erind hoxha</h1>
+          </div>
+        </motion.div>
         <div>
           <div className={styles.box}>
-            <div className={styles.innerBox}>
-              <p className="description">Front-end developer</p>
-              <p className="description">
-                Currently working at{' '}
-                <a
-                  className="link line-through semibold"
-                  href="#"
-                  onMouseEnter={() => setCursorVariantFn('text')}
-                  onMouseLeave={() => setCursorVariantFn('default')}
-                >
-                  Kablamo
-                </a>
-              </p>
-            </div>
-            <div className={styles.innerBox}>
-              <p className="description">Sydney, Australia</p>
+            <motion.div
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 1 }}
+            >
+              <div className={styles.innerBox}>
+                <p className="description">Front-end developer</p>
+                <p className="description mb-0">
+                  Currently working at{' '}
+                  <a
+                    className="link line-through semibold"
+                    href="#"
+                    onMouseEnter={() => setCursorVariantFn('text')}
+                    onMouseLeave={() => setCursorVariantFn('default')}
+                  >
+                    Kablamo
+                  </a>
+                </p>
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 1 }}
+            >
+              <div className={styles.innerBox}>
+                <p className="description">Sydney, Australia</p>
 
-              <p className="description">
-                <a
-                  className="link line-through semibold"
-                  href="#"
-                  onMouseEnter={() => setCursorVariantFn('text')}
-                  onMouseLeave={() => setCursorVariantFn('default')}
-                >
-                  hello@erindhoxha.dev
-                </a>
-              </p>
-            </div>
+                <p className="description mb-0">
+                  <a
+                    className="link line-through semibold"
+                    href="#"
+                    onMouseEnter={() => setCursorVariantFn('text')}
+                    onMouseLeave={() => setCursorVariantFn('default')}
+                  >
+                    hello@erindhoxha.dev
+                  </a>
+                </p>
+              </div>
+            </motion.div>
           </div>
         </div>
       </div>
--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -9,37 +9,33 @@ const Projects = () => {
   const { setCursorVariantFn } = useCursorContext();
   return (
     <div className={`${styles.projectsContainer} container-fluid`}>
-      <div className="row">
-        <div className="row">
-          <h1 className={styles.h1}>Some things I&apos;ve built</h1>
-        </div>
-        <div
-          className={`col-12 col-md-6 col-lg-4 m-0 p-1 ${styles.portfolioContainer}`}
+      <h1 className={styles.h1}>Some things I&apos;ve built</h1>
+      <div
+        className={`col-12 col-md-6 col-lg-4 m-0 p-1 ${styles.portfolioContainer}`}
+      >
+        <motion.div
+          style={{ height: '100%' }}
+          animate={{ y: 0 }}
+          transition={{
+            y: { duration: 0.4 },
+            default: { ease: 'easeInOut' },
+          }}
         >
-          <motion.div
-            style={{ height: '100%' }}
-            animate={{ y: 0 }}
-            transition={{
-              y: { duration: 0.4 },
-              default: { ease: 'easeInOut' },
-            }}
-          >
-            <div className={styles.textContainer}>
-              <Link
-                onMouseEnter={() => setCursorVariantFn('textInImage')}
-                onMouseLeave={() => setCursorVariantFn('default')}
-                href="#"
-                className="link link-light"
-              >
-                <lead>2023</lead>
-                <h1>Fishmarkets</h1>
-                <p>react - nextjs - redux - figma - ai</p>
-              </Link>
-            </div>
+          <div className={styles.textContainer}>
+            <Link
+              onMouseEnter={() => setCursorVariantFn('textInImage')}
+              onMouseLeave={() => setCursorVariantFn('default')}
+              href="#"
+              className="link link-light"
+            >
+              <lead>2023</lead>
+              <h1>Fishmarkets</h1>
+              <p>react - nextjs - redux - figma - ai</p>
+            </Link>
+          </div>
 
-            <Image className={styles.img} alt="portfolio image" src={Img} />
-          </motion.div>
-        </div>
+          <Image className={styles.img} alt="portfolio image" src={Img} />
+        </motion.div>
       </div>
     </div>
   );
