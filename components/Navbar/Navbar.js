320cd1a99ad8943ed00ad059210d77900b397520
erindhoxha <hello@erindhoxha.dev>
2023-02-03 22:27:44 +1100

    spin

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -58,7 +58,12 @@ const Navbar = () => {
         stroke-linecap="round"
         stroke-linejoin="round"
       >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path
+          className={navbarStyles.spin}
+          stroke="none"
+          d="M0 0h24v24H0z"
+          fill="none"
+        />
         <circle cx="12" cy="12" r="4" />
         <line
           className={theme === 'dark' ? navbarStyles.line : ''}
@@ -68,7 +73,10 @@ const Navbar = () => {
           y2="21"
         />
         <path d="M16 12a4 4 0 0 0 -4 -4m-2.834 1.177a4 4 0 0 0 5.66 5.654" />
-        <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
+        <path
+          className={navbarStyles.spin}
+          d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"
+        />
       </svg>
     </>
   );
