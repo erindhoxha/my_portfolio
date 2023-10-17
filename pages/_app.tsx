4691729076a8745981f084ed910c635f1a8ca2a1
erindhoxha <erind.cbh@gmail.com>
2023-10-17 21:25:53 +1100

    Seo fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,117 +6,117 @@ import { createContext, useContext, useEffect, useState } from 'react';
 import { Analytics } from '@vercel/analytics/react';
 
 export const CursorContext = createContext({
-  mousePosition: {
-    x: undefined,
-    y: undefined,
-  },
-  cursorVariant: '',
-  setCursorVariantFn: (s: string) => null,
-  setCursorText: (s: string) => null,
-  variants: {},
+ mousePosition: {
+  x: undefined,
+  y: undefined,
+ },
+ cursorVariant: '',
+ setCursorVariantFn: (s: string) => null,
+ setCursorText: (s: string) => null,
+ variants: {},
 });
 
 export default function App({ Component, pageProps }) {
-  const [mousePosition, setMousePosition] = useState({
-    x: 0,
-    y: 0,
+ const [mousePosition, setMousePosition] = useState({
+  x: 0,
+  y: 0,
+ });
+ const [cursorVariant, setCursorVariant] = useState('default');
+ const [cursorText, setCursorText] = useState('');
+ const variants = {
+  default: {
+   x: mousePosition.x - 25,
+   y: mousePosition.y - 25,
+  },
+  text: {
+   height: 70,
+   width: 70,
+   x: mousePosition.x - 35,
+   y: mousePosition.y - 35,
+   fontSize: '15px',
+   backgroundColor: 'var(--text-hex)',
+  },
+  test: {
+   height: 70,
+   width: 70,
+   x: mousePosition.x - 35,
+   y: mousePosition.y - 35,
+  },
+  textInImage: {
+   height: 100,
+   width: 100,
+   x: mousePosition.x - 50,
+   y: mousePosition.y - 50,
+  },
+ };
+ const mouseMove = (e) => {
+  setMousePosition({
+   x: e.clientX,
+   y: e.clientY,
   });
-  const [cursorVariant, setCursorVariant] = useState('default');
-  const [cursorText, setCursorText] = useState('');
-  const variants = {
-    default: {
-      x: mousePosition.x - 25,
-      y: mousePosition.y - 25,
-    },
-    text: {
-      height: 70,
-      width: 70,
-      x: mousePosition.x - 35,
-      y: mousePosition.y - 35,
-      fontSize: '15px',
-      backgroundColor: 'var(--text-hex)',
-    },
-    test: {
-      height: 70,
-      width: 70,
-      x: mousePosition.x - 35,
-      y: mousePosition.y - 35,
-    },
-    textInImage: {
-      height: 100,
-      width: 100,
-      x: mousePosition.x - 50,
-      y: mousePosition.y - 50,
-    },
+ };
+
+ useEffect(() => {
+  window.addEventListener('mousemove', mouseMove);
+
+  return () => {
+   window.removeEventListener('mousemove', mouseMove);
   };
-  const mouseMove = (e) => {
-    setMousePosition({
-      x: e.clientX,
-      y: e.clientY,
-    });
-  };
+ }, []);
 
-  useEffect(() => {
-    window.addEventListener('mousemove', mouseMove);
+ function setCursorVariantFn(string) {
+  setCursorVariant(string);
+ }
 
-    return () => {
-      window.removeEventListener('mousemove', mouseMove);
-    };
-  }, []);
+ const context = {
+  mousePosition,
+  cursorVariant,
+  setCursorVariantFn,
+  setCursorText,
+  cursorText,
+  variants,
+ };
 
-  function setCursorVariantFn(string) {
-    setCursorVariant(string);
+ // const { scrollYProgress } = useScroll();
+
+ // const scaleX = useSpring(scrollYProgress, {
+ //   stiffness: 100,
+ //   damping: 30,
+ //   restDelta: 0.001,
+ // });
+
+ const [theme, setTheme] = useState('');
+ useEffect(() => {
+  if (
+   localStorage.getItem('theme') === 'dark' ||
+   localStorage.getItem('theme') === '' ||
+   localStorage.getItem('theme') === null
+  ) {
+   document.documentElement.setAttribute('data-theme', 'dark');
+   setTheme('dark');
+  } else {
+   document.documentElement.setAttribute('data-theme', 'light');
+   setTheme('light');
   }
+ }, []);
 
-  const context = {
-    mousePosition,
-    cursorVariant,
-    setCursorVariantFn,
-    setCursorText,
-    cursorText,
-    variants,
-  };
-
-  // const { scrollYProgress } = useScroll();
-
-  // const scaleX = useSpring(scrollYProgress, {
-  //   stiffness: 100,
-  //   damping: 30,
-  //   restDelta: 0.001,
-  // });
-
-  const [theme, setTheme] = useState('');
-  useEffect(() => {
-    if (
-      localStorage.getItem('theme') === 'dark' ||
-      localStorage.getItem('theme') === '' ||
-      localStorage.getItem('theme') === null
-    ) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-      setTheme('dark');
-    } else {
-      document.documentElement.setAttribute('data-theme', 'light');
-      setTheme('light');
-    }
-  }, []);
-
-  return (
-    <>
-      {/* <motion.div className="progress-bar" style={{ scaleX }} /> */}
-      <CursorContext.Provider value={context}>
-        <Cursor
-          cursorText={cursorText}
-          variants={variants}
-          cursorVariant={cursorVariant}
-        />
-        <Analytics />
-        <Component data-theme={theme || 'dark'} {...pageProps} />
-      </CursorContext.Provider>
-    </>
-  );
+ return (
+  <>
+   {/* <motion.div className="progress-bar" style={{ scaleX }} /> */}
+   <CursorContext.Provider value={context}>
+    <Cursor
+     cursorText={cursorText}
+     variants={variants}
+     cursorVariant={cursorVariant}
+    />
+    <Analytics />
+    <Component data-theme={theme || 'dark'} {...pageProps} />
+   </CursorContext.Provider>
+  </>
+ );
 }
 
 export const useCursorContext = () => {
-  const context = useContext(CursorContext);
-  return context;
+ const context = useContext(CursorContext);
+ return context;
 };
--- pages/work/[slug].tsx
+++ pages/work/[slug].tsx
@@ -8,92 +8,100 @@ import { motion } from 'framer-motion';
 import Navbar from '../../components/Navbar/Navbar';
 import Projects from '../../components/Projects/Projects';
 import Image from 'next/image';
+import Head from 'next/head';
 
 export default function Post({ post, morePosts, preview }) {
-  const router = useRouter();
-  if (!router.isFallback && !post?.slug) {
-    return "There's no slug";
-  }
+ const router = useRouter();
+ if (!router.isFallback && !post?.slug) {
+  return "There's no slug";
+ }
 
-  let boxVariants = {};
-  let initialLeft = {};
-  let initialRight = {};
-  if (typeof window !== 'undefined') {
-    const isMobile = window.innerWidth < 768; //Add the width you want to check for here (now 768px)
-    if (!isMobile) {
-      boxVariants = {
-        default: {
-          opacity: 1,
-          y: 0,
-        },
-      };
-      initialLeft = { opacity: 0, y: -40 };
-      initialRight = { opacity: 0, y: 40 };
-    } //if the width >= 768px, boxVariants will be empty, resulting in no animation
-    //you need to refresh the page, it doesn't work when you resize it!
-  }
+ let boxVariants = {};
+ let initialLeft = {};
+ let initialRight = {};
+ if (typeof window !== 'undefined') {
+  const isMobile = window.innerWidth < 768; //Add the width you want to check for here (now 768px)
+  if (!isMobile) {
+   boxVariants = {
+    default: {
+     opacity: 1,
+     y: 0,
+    },
+   };
+   initialLeft = { opacity: 0, y: -40 };
+   initialRight = { opacity: 0, y: 40 };
+  } //if the width >= 768px, boxVariants will be empty, resulting in no animation
+  //you need to refresh the page, it doesn't work when you resize it!
+ }
 
-  return (
-    <>
-      <div className="slug container-fluid">
-        <Navbar />
-        <div className={styles.slug}>
-          <div className={`${styles.slugContainer} bg`}>
-            <div className={styles.navMobile}>
-              <Navbar />
-            </div>
+ const title = post.title.charAt(0).toUpperCase() + post.title.slice(1);
 
-            <NavLeft />
-            <div className={styles.slugContainerInner}>
-              <motion.div
-                variants={boxVariants}
-                animate="default"
-                transition={{ duration: 0.5, delay: 0.4 }}
-                initial={initialRight}
-              >
-                <p className={styles.fadedP}>showcase_</p>
-                <h1>{post.title}</h1>
-              </motion.div>
+ return (
+  <>
+   <Head>
+    <title>{title} - Case study</title>
+    <meta name="description" content={`Case study about ${title}`} />
+    <meta property="og:title" content={`${title} - Case study`} />
+   </Head>
+   <div className="slug container-fluid">
+    <Navbar />
+    <div className={styles.slug}>
+     <div className={`${styles.slugContainer} bg`}>
+      <div className={styles.navMobile}>
+       <Navbar />
+      </div>
 
-              <motion.div
-                variants={boxVariants}
-                animate="default"
-                transition={{ duration: 0.5, delay: 0.6 }}
-                initial={initialRight}
-              >
-                <h2 className={`mt-5`}>tools used</h2>
-                <p className={styles.fadedP}>{post.tools}</p>
-              </motion.div>
+      <NavLeft />
+      <div className={styles.slugContainerInner}>
+       <motion.div
+        variants={boxVariants}
+        animate="default"
+        transition={{ duration: 0.5, delay: 0.4 }}
+        initial={initialRight}
+       >
+        <p className={styles.fadedP}>showcase_</p>
+        <h1>{post.title}</h1>
+       </motion.div>
 
-              <motion.div
-                variants={boxVariants}
-                animate="default"
-                transition={{ duration: 0.5, delay: 0.8 }}
-                initial={initialRight}
-              >
-                <h2 className={`mt-5`}>year</h2>
-                <p className={styles.fadedP}>{post.year}</p>
-              </motion.div>
+       <motion.div
+        variants={boxVariants}
+        animate="default"
+        transition={{ duration: 0.5, delay: 0.6 }}
+        initial={initialRight}
+       >
+        <h2 className={`mt-5`}>tools used</h2>
+        <p className={styles.fadedP}>{post.tools}</p>
+       </motion.div>
 
-              {post.cta && (
-                <motion.div
-                  variants={boxVariants}
-                  animate="default"
-                  transition={{ duration: 0.5, delay: 1 }}
-                  initial={initialRight}
-                  className={`${styles.cta} mt-4`}
-                >
-                  <a
-                    href={post.url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="btn btn-outline rounded py-3 px-5"
-                  >
-                    {post.urlName}
-                  </a>
-                </motion.div>
-              )}
-              {/* 
+       <motion.div
+        variants={boxVariants}
+        animate="default"
+        transition={{ duration: 0.5, delay: 0.8 }}
+        initial={initialRight}
+       >
+        <h2 className={`mt-5`}>year</h2>
+        <p className={styles.fadedP}>{post.year}</p>
+       </motion.div>
+
+       {post.cta && (
+        <motion.div
+         variants={boxVariants}
+         animate="default"
+         transition={{ duration: 0.5, delay: 1 }}
+         initial={initialRight}
+         className={`${styles.cta} mt-4`}
+        >
+         <a
+          href={post.url}
+          target="_blank"
+          rel="noreferrer"
+          className="btn btn-outline rounded py-3 px-5"
+         >
+          {post.urlName}
+         </a>
+        </motion.div>
+       )}
+       {/* 
               {post.cta2 && (
                 <motion.div
                   variants={boxVariants}
@@ -113,92 +121,90 @@ export default function Post({ post, morePosts, preview }) {
                 </motion.div>
               )} */}
 
-              <div className={styles.nameContainer}>
-                <PortfolioLeftDescription
-                  title=""
-                  role={post.role}
-                  cta={post.cta}
-                  link={post.urlName}
-                  href={post.url}
-                  location={post.location}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+       <div className={styles.nameContainer}>
+        <PortfolioLeftDescription
+         title=""
+         role={post.role}
+         cta={post.cta}
+         link={post.urlName}
+         href={post.url}
+         location={post.location}
+        />
+       </div>
       </div>
-      <div
-        id="scrollto"
-        className={`${styles.showcaseContainer} container-fluid`}
-      >
-        {post.mockup ? (
-          <div className="text-center pb-4 mb-4">
-            <Image
-              src={post.mockup}
-              alt={post.title}
-              width={1200}
-              height={1200}
-              style={{
-                width: '100% !important',
-                height: '100%',
-                maxWidth: '780px',
-                marginTop: '0px',
-              }}
-            />
-          </div>
-        ) : null}
-        <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
-      </div>
-      <Projects />
-    </>
-  );
+     </div>
+    </div>
+   </div>
+   <div id="scrollto" className={`${styles.showcaseContainer} container-fluid`}>
+    {post.mockup ? (
+     <div className="text-center pb-4 mb-4">
+      <Image
+       src={post.mockup}
+       alt={post.title}
+       width={1200}
+       height={1200}
+       style={{
+        width: '100% !important',
+        height: '100%',
+        maxWidth: '780px',
+        marginTop: '0px',
+       }}
+      />
+     </div>
+    ) : null}
+    <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+   </div>
+   <Projects />
+  </>
+ );
 }
 
 export async function getStaticProps({ params }) {
-  const post = getPostBySlug(params.slug, [
-    'title',
-    'tools',
-    'role',
-    'mockup',
-    'cta',
-    'url',
-    'urlName',
-    'cta2',
-    'url2',
-    'urlName2',
-    'date',
-    'year',
-    'location',
-    'slug',
-    'author',
-    'content',
-    'ogImage',
-    'coverImage',
-  ]);
+ const post = getPostBySlug(params.slug, [
+  'title',
+  'tools',
+  'role',
+  'mockup',
+  'cta',
+  'url',
+  'urlName',
+  'cta2',
+  'url2',
+  'urlName2',
+  'date',
+  'year',
+  'location',
+  'slug',
+  'author',
+  'content',
+  'ogImage',
+  'coverImage',
+ ]);
 
-  const content = await markdownToHtml(post.content || '');
+ const content = await markdownToHtml(post.content || '');
 
-  return {
-    props: {
-      post: {
-        ...post,
-        content,
-      },
-    },
-  };
+ return {
+  props: {
+   post: {
+    ...post,
+    content,
+   },
+   title: post.title,
+  },
+ };
 }
 
 export async function getStaticPaths() {
-  const posts = getAllPosts(['slug']);
+ const posts = getAllPosts(['slug']);
 
-  return {
-    paths: posts.map((post) => {
-      return {
-        params: {
-          slug: post.slug,
-        },
-      };
-    }),
-    fallback: false,
-  };
+ return {
+  paths: posts.map((post) => {
+   return {
+    params: {
+     slug: post.slug,
+    },
+   };
+  }),
+  fallback: false,
+ };
 }
