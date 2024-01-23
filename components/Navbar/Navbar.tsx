f1a22a1c402038db0e074dc9e012c0034cded9dc
erindhoxha <hello@erindhoxha.dev>
2024-01-24 00:53:38 +1100

    Fix mobile issue with safari, showing bottom sheet

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -34,56 +34,58 @@ const Navbar = () => {
   };
   return (
     <>
-      <header className={navbarStyles.header}>
-        <nav className={`${navbarStyles.nav} container-fluid`}>
-          <motion.div initial={{ x: 24 }}>
-            <Logo />
-          </motion.div>
-        </nav>
-        <svg
-          tabIndex={0}
-          aria-label="Switch theme"
-          className={`${navbarStyles.sun} ${
-            theme === "dark" ? navbarStyles.sunDark : ""
-          }`}
-          style={{
-            outline: "none",
-          }}
-          xmlns="http://www.w3.org/2000/svg"
-          width="44"
-          height="44"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          focusable
-          stroke="#2c3e50"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          onClick={() => {
-            switchTheme();
-          }}
-        >
-          <path
-            className={navbarStyles.spin}
-            stroke="none"
-            d="M0 0h24v24H0z"
+      <div className="relative">
+        <header className={navbarStyles.header}>
+          <nav className={`${navbarStyles.nav} container-fluid`}>
+            <motion.div initial={{ x: 24 }}>
+              <Logo />
+            </motion.div>
+          </nav>
+          <svg
+            tabIndex={0}
+            aria-label="Switch theme"
+            className={`${navbarStyles.sun} ${
+              theme === "dark" ? navbarStyles.sunDark : ""
+            }`}
+            style={{
+              outline: "none",
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            width="44"
+            height="44"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            focusable
+            stroke="#2c3e50"
             fill="none"
-          />
-          <circle cx="12" cy="12" r="4" />
-          <line
-            className={theme === "light" ? navbarStyles.line : ""}
-            x1="3"
-            y1="3"
-            x2="21"
-            y2="21"
-          />
-          <path d="M16 12a4 4 0 0 0 -4 -4m-2.834 1.177a4 4 0 0 0 5.66 5.654" />
-          <path
-            className={navbarStyles.spin}
-            d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"
-          />
-        </svg>
-      </header>
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            onClick={() => {
+              switchTheme();
+            }}
+          >
+            <path
+              className={navbarStyles.spin}
+              stroke="none"
+              d="M0 0h24v24H0z"
+              fill="none"
+            />
+            <circle cx="12" cy="12" r="4" />
+            <line
+              className={theme === "light" ? navbarStyles.line : ""}
+              x1="3"
+              y1="3"
+              x2="21"
+              y2="21"
+            />
+            <path d="M16 12a4 4 0 0 0 -4 -4m-2.834 1.177a4 4 0 0 0 5.66 5.654" />
+            <path
+              className={navbarStyles.spin}
+              d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"
+            />
+          </svg>
+        </header>
+      </div>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,77 +1,106 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Projects from '../components/Projects/Projects';
-import NavLeft from '../components/NavLeft/NavLeft';
-import Name from '../components/Name/Name';
-import NavRight from '../components/NavRight/NavRight';
-import Blogs from '../components/Blogs/Blogs';
-import Navbar from '../components/Navbar/Navbar';
-import Contact from '../components/Contact/Contact';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Projects from "../components/Projects/Projects";
+import NavLeft from "../components/NavLeft/NavLeft";
+import Name from "../components/Name/Name";
+import NavRight from "../components/NavRight/NavRight";
+import Blogs from "../components/Blogs/Blogs";
+import Navbar from "../components/Navbar/Navbar";
+import Contact from "../components/Contact/Contact";
+import { useEffect } from "react";
 
 export default function Home() {
- return (
-  <>
-   <Head>
-    <title>Erind Hoxha | Frontend Engineer | UI/UX | Portfolio</title>
-    <meta name="description" content="Frontend Engineer | UX/UI | Portfolio" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/favicon.ico?v=1" type="image/x-icon" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-    <meta
-     name="thumbnail"
-     content="http://erindhoxha-portfolio.vercel.app/api/og"
-    />
-    <link rel="manifest" href="/site.webmanifest" />
-    <meta name="msapplication-TileColor" content="#050505" />
-    <meta name="theme-color" content="#050505" />
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#070707" />
-    <meta
-     property="og:title"
-     content="Erind Hoxha | Frontend Engineer | UI/UX | Portfolio"
-    />
-    <meta property="og:site_name" content="erindhoxha.dev" />
-    <meta property="og:url" content="https://erindhoxha.dev" />
-    <meta
-     property="og:description"
-     content="Frontend Engineer | UX/UI | Portfolio"
-    />
-    <meta property="og:type" content="website" />
-    <meta
-     property="og:image"
-     content="http://erindhoxha-portfolio.vercel.app/api/og"
-    />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="https://erindhoxha.dev" />
-    <meta name="twitter:creator" content="@erindhoxha" />
-    <meta name="twitter:title" content="Erind Hoxha Portfolio" />
-    <meta
-     name="twitter:description"
-     content="Frontend Engineer | UX/UI | Portfolio"
-    />
-    <meta
-     name="twitter:image"
-     content="http://erindhoxha-portfolio.vercel.app/api/og"
-    />
-   </Head>
-   <main className={`${styles.main}`}>
-    <div className="home container-fluid">
-     <Navbar />
-     <div className={`container-fluid p-0 h-100`}>
-      <NavLeft />
-      <div className={styles.navAndNameContainer}>
-       <NavRight />
-       <Name />
-      </div>
-     </div>
-    </div>
+  function adjustHeight() {
+    var vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
 
-    {/* <SocialBar /> */}
-    <Projects />
-    <Blogs />
-    <Contact />
-   </main>
-  </>
- );
+  useEffect(() => {
+    // Run the function when the page is loaded and when the window is resized
+    window.addEventListener("load", adjustHeight);
+    window.addEventListener("resize", adjustHeight);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Erind Hoxha | Frontend Engineer | UI/UX | Portfolio</title>
+        <meta
+          name="description"
+          content="Frontend Engineer | UX/UI | Portfolio"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico?v=1" type="image/x-icon" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <meta
+          name="thumbnail"
+          content="http://erindhoxha-portfolio.vercel.app/api/og"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#050505" />
+        <meta name="theme-color" content="#050505" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#070707" />
+        <meta
+          property="og:title"
+          content="Erind Hoxha | Frontend Engineer | UI/UX | Portfolio"
+        />
+        <meta property="og:site_name" content="erindhoxha.dev" />
+        <meta property="og:url" content="https://erindhoxha.dev" />
+        <meta
+          property="og:description"
+          content="Frontend Engineer | UX/UI | Portfolio"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="http://erindhoxha-portfolio.vercel.app/api/og"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="https://erindhoxha.dev" />
+        <meta name="twitter:creator" content="@erindhoxha" />
+        <meta name="twitter:title" content="Erind Hoxha Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Frontend Engineer | UX/UI | Portfolio"
+        />
+        <meta
+          name="twitter:image"
+          content="http://erindhoxha-portfolio.vercel.app/api/og"
+        />
+      </Head>
+      <main className={`${styles.main}`}>
+        <div className="home container-fluid">
+          <Navbar />
+          <div className={`container-fluid p-0`}>
+            <NavLeft />
+            <div className={styles.navAndNameContainer}>
+              <NavRight />
+              <Name />
+            </div>
+          </div>
+        </div>
+
+        {/* <SocialBar /> */}
+        <Projects />
+        <Blogs />
+        <Contact />
+      </main>
+    </>
+  );
 }
